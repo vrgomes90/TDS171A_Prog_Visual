af8d39c7a3a0b89a6b454d6aa22c5d262546662d
Lab. Desenvolvimento <Lab. Desenvolvimento@RB-LAB102-12>
2017-08-08 20:32:10 -0300

    Add project files.

---
+++ TDS171A_Diogo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TDS171A_Diogo.MvcApplication" Language="C#" %>
---
+++ TDS171A_Diogo/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDS171A_Diogo.Models
+{
+    public class Category
+    {
+    }
+}
